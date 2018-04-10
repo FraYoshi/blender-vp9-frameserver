3543a2c09b6c40252cae08d4feaa020dbf6d73b8
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2018-04-10 10:41:15 +0200

    THREADS variable addition

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # blender-vp9-frameserver
 render video to VP9 via **ffmpeg** using the standard **frameserver** output of Blender.
 
-This is a revision of the [original code](https://wiki.blender.org/index.php/Doc_talk:2.6/Manual/Render/Output/Frameserver) posted by zagaeski on the official wiki of blender.
+This is a revision of the [original code](https://wiki.blender.org/index.php/Doc_talk:2.6/Manual/Render/Output/Frameserver) posted by **zagaeski** on the official wiki of blender.
 I remade this to export in webm format and vp9 codec including the audio.
 
 # HOW TO
 ## NO AUDIO
 - copy the script or clone the repo
 - set it executable with `chmod +x`
-- _(optional)_ open the script and set the number of threads you want to use. follow the bottom comments
+- _(optional)_ open the script and set the number of CPU THREADS you want to use on the top.
 - open blender and set the output to **Frame Server**
 - execute the script `./blender-vp9-frameserver.sh`
 
--- blender-vp9-frameserver.sh
+++ blender-vp9-frameserver.sh
@@ -2,6 +2,8 @@
 BLENDER=http://localhost:8080
 #AUDIO=/insert/here/the/path/to/your/audio/file.ext
 OUTPUT=/tmp/output.webm
+# set the number of threads manually.. actually it doesn't multi-thread the conversion if not set
+THREADS=4
 eval `wget ${BLENDER}/info.txt -O - 2>/dev/null |
     while read key val ; do
       echo R_$key=$val  
@@ -12,9 +14,8 @@ i=$R_start
        wget ${BLENDER}/images/ppm/$i.ppm -O - 2>/dev/null
        i=$(($i+1))
   done
-# set the number of threads manually.. actually it doesn't multi-thread the conversion if not set
 # the CRF factor is suggested in the range 15-35. 15 for the highest quality
-} | ffmpeg -vcodec ppm -f image2pipe -r $R_rate -i pipe:0 -vcodec libvpx-vp9 -crf 30 -b 0 -threads 4 $OUTPUT
+} | ffmpeg -vcodec ppm -f image2pipe -r $R_rate -i pipe:0 -vcodec libvpx-vp9 -crf 30 -b 0 -threads $THREADS $OUTPUT
 # for audio addition uncomment the line below and set the AUDIO variable at the top
-#} | ffmpeg -vcodec ppm -f image2pipe -r $R_rate -i pipe:0 -i $AUDIO -vcodec libvpx-vp9 -crf 30 -b 0 -acodec libopus -vbr on -threads 4 $OUTPUT
+#} | ffmpeg -vcodec ppm -f image2pipe -r $R_rate -i pipe:0 -i $AUDIO -vcodec libvpx-vp9 -crf 30 -b 0 -acodec libopus -vbr on -threads $THREADS $OUTPUT
 wget ${BLENDER}/close.txt -O - 2>/dev/null >/dev/null
